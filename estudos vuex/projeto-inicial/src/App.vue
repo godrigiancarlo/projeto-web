538f0404ac7b986474b1d0626118bdb8d81d693e
Your Name <you@example.com>
2020-03-02 21:45:59 -0300

    parte1

---
+++ estudos vuex/projeto-inicial/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Contador/>
+  </div>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+</style>
